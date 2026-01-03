b195ccb227f17b8a24dd3cd8056411009ad794df
Nadja Zoric <zoric.n2003@gmail.com>
2026-01-03 20:26:19 +0100

    Adjust parameter values - 1.0 MAE

--- script.py
+++ script.py
@@ -20,9 +20,9 @@ def preprocess_video(video_name, results):
     video_path = os.path.join(DATA_DIR, video_name + ".mp4")
 
     cap = cv2.VideoCapture(video_path)
-    sum_of_bones = 0
+    num_bones = 0
     is_circle_active = False
-    skipped_frames_num = 0
+    num_skipped_frames = 0
     
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -35,10 +35,10 @@ def preprocess_video(video_name, results):
 
         hsv = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2HSV)
         
-        lower_gold = np.array([10, 0, 180])
-        upper_gold = np.array([30, 255, 255])
+        lower_mask = np.array([10, 0, 180])
+        upper_mask = np.array([30, 255, 255])
         
-        mask = cv2.inRange(hsv, lower_gold, upper_gold)
+        mask = cv2.inRange(hsv, lower_mask, upper_mask)
         
         mask = cv2.medianBlur(mask, 5)
         circles = cv2.HoughCircles(
@@ -48,24 +48,23 @@ def preprocess_video(video_name, results):
             minDist=50,
             param1=50, 
             param2=15, 
-            minRadius=30, 
+            minRadius=20, 
             maxRadius=100
         )
         if circles is not None:
-            circles = np.uint16(np.around(circles))
-            skipped_frames_num = 0
+            num_skipped_frames = 0
             if not is_circle_active:
-                sum_of_bones += 1
+                num_bones += 1
                 is_circle_active = True
         else:
-            skipped_frames_num += 1
-            if skipped_frames_num >= 5:
+            num_skipped_frames += 1
+            if num_skipped_frames >= 5:
                 is_circle_active = False
+
     cap.release()
-    results[video_name] = sum_of_bones
+    results[video_name] = num_bones
 
 if __name__ == "__main__":
-    # start_time = cv2.getTickCount()
     expected_results = load_expected_results()
     data_directory = os.path.dirname(os.path.abspath(__file__))
     results = {}
@@ -74,7 +73,3 @@ if __name__ == "__main__":
 
     mae = np.mean([np.abs(results[video] - expected_results[video]) for video in expected_results.keys()])
     print(mae)
-    # print("Results:", results)
-    # end_time = cv2.getTickCount()
-    # time = (end_time - start_time) / cv2.getTickFrequency()
-    # print(f"Total processing time: {time} seconds")
